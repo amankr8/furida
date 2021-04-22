bbe80ca8eda42d8d49b1a676b392e6b83aeac64b
Aman Kumar <rx8aman@gmail.com>
2021-04-22 17:24:35 +0530

    url change

--- client/src/api/posts.js
+++ client/src/api/posts.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const url = 'https://furida.herokuapp.com/posts'
-// const url = 'http://localhost:8080/posts'
+// const url = 'https://furida.herokuapp.com/posts'
+const url = 'http://localhost:8080/posts'
 
 export const getPosts = () => axios.get(url)
 export const createPost = (postData) => axios.post(url + '/create', postData)
