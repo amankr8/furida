d26ca596ec92481c89127eca90547a88c2c322d6
Aman Kumar <rx8aman@gmail.com>
2021-04-27 19:47:57 +0530

    debugged

--- client/src/components/Console/Messages/Message.js
+++ client/src/components/Console/Messages/Message.js
@@ -20,42 +20,42 @@ const Message = (props) => {
             </div>
 
             {/* MessageModal */}
-            <div class="modal fade" id={"view" + props.msg._id} tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLongTitle">MESSAGE</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <div className="modal fade" id={"view" + props.msg._id} tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLongTitle">MESSAGE</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
-                        <div class="modal-body">
+                        <div className="modal-body">
                             {props.msg.message}
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Close</button>
-                            <a href={"mailto:" + props.msg.email} class="btn btn-danger">Respond</a>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-outline-secondary" data-dismiss="modal">Close</button>
+                            <a href={"mailto:" + props.msg.email} className="btn btn-danger">Respond</a>
                         </div>
                     </div>
                 </div>
             </div>
 
             {/* DeleteModal */}
-            <div class="modal fade" id={"del" + props.msg._id} tabindex="-1" role="dialog" aria-labelledby="messageModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="messageModalLabel">CONFIRMATION</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <div className="modal fade" id={"del" + props.msg._id} tabIndex="-1" role="dialog" aria-labelledby="messageModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="messageModalLabel">CONFIRMATION</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
-                        <div class="modal-body">
+                        <div className="modal-body">
                             Are you sure you want to delete this message?
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-danger" data-dismiss="modal" onClick={() => dispatch(deleteMessage(props.msg._id))}>Delete</button>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-outline-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-danger" data-dismiss="modal" onClick={() => dispatch(deleteMessage(props.msg._id))}>Delete</button>
                         </div>
                     </div>
                 </div>
