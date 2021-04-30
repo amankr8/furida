3b426694df6184c89b85958813a8e6d5fe919d85
Aman Kumar <rx8aman@gmail.com>
2021-04-30 22:55:31 +0530

    image display added

--- client/src/components/Console/Posts/DisplayPosts/Post.js
+++ client/src/components/Console/Posts/DisplayPosts/Post.js
@@ -27,7 +27,7 @@ const Post = (props) => {
         <div className="col-lg-6">
             {/* Post */}
             <div className="card mb-3">
-                <img className="card-img-top" src="https://picsum.photos/480/360" alt="Card" />
+                <img className="card-img-top" src={"http://localhost:8080/uploads/" + props.post.img} alt="Card" />
                 <div className="card-body">
                     <p className="card-text">{props.post.desc}</p>
                     <div className="d-flex justify-content-between align-items-center">
--- client/src/components/Webpage/Posts/Post.js
+++ client/src/components/Webpage/Posts/Post.js
@@ -5,7 +5,7 @@ const Post = (props) => {
     return (
         <div className="col-lg-4 col-md-6">
             <div className="card border-0 shadow-sm mb-4">
-                <img className="card-img-top" src="https://picsum.photos/480/360" alt="Card" />
+                <img className="card-img-top" src={ "http://localhost:8080/uploads/" + props.post.img } alt="Card" />
                 <div className="card-body">
                     <p className="card-text">{props.post.desc}</p>
                     <div className="d-flex justify-content-between align-items-center">
