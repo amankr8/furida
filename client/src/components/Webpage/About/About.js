a97ac7b8cd52f1ed9d99365be81fd07a0d099ce5
Aman Kumar <rx8aman@gmail.com>
2021-05-06 17:57:48 +0530

    color added

--- client/src/components/Webpage/About/About.js
+++ client/src/components/Webpage/About/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div className="bg-light py-5 rounded">
+        <div className="bg-light py-5 border rounded">
             <h4 className="text-center pt-4">ABOUT THE ORGANISATION</h4>
             <div className="container" style={{maxWidth: "60rem"}}>
                 <hr/>
--- client/src/components/Webpage/Webpage.js
+++ client/src/components/Webpage/Webpage.js
@@ -8,15 +8,15 @@ import Footer from '../modules/Footer'
 
 const Cover = () => {
     return (
-        <div>
-            <img src="https://picsum.photos/1280/720" className="img-fluid" alt="Cover" />
+        <div className="border rounded">
+            <img src="https://picsum.photos/1280/720" className="img-fluid rounded" alt="Cover" />
         </div>
     )
 }
 
 const Map = () => {
     return (
-        <div className="border rounded">
+        <div className="bg-white border rounded">
             <iframe title="Google Maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7570.283295787509!2d86.22416387383286!3d22.825200134855145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb58ae6cf9ef175c4!2sFURIDA!5e0!3m2!1sen!2sin!4v1618419317130!5m2!1sen!2sin" height="450" style={{border: "0", width: "100%"}} allowFullScreen="" loading="lazy" />
         </div>
     )
