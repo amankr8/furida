e03c3562055b655ae1bc85a09ec5c6c66520edf8
Aman Kumar <rx8aman@gmail.com>
2021-05-31 00:26:28 +0530

    jwt debug

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -3,17 +3,18 @@ const User = require('../components/models/User')
 
 const auth = async (req, res, next) => {
     try {
+        // Check if token exists
         const token = req.headers.authorization
         if (!token) {
-            res.status(401).json({ message: 'Authorization denied'})
+            return res.status(401).json({ message: 'Authorization denied'})
         }
-        else {
-            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
-            const user = await User.findById(decoded.id)
-            req.user = user
 
-            next()
-        }
+        // Verify token 
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+        const user = await User.findById(decoded.id)
+        req.user = user
+
+        next()
     } catch (err) {
         console.error(err)
     }
