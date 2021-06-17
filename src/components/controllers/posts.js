ce5bf9f137bd3efdb3b36470b453ffac3f082183
Aman Kumar <rx8aman@gmail.com>
2021-06-16 20:03:37 -0700

    storage added

--- src/components/controllers/posts.js
+++ src/components/controllers/posts.js
@@ -1,5 +1,5 @@
 const Post = require('../models/Post')
-const { deleteFile, deleteFiles } = require('../../helpers')
+const { deleteFile, deleteFiles } = require('../../helpers/storage')
 const { uploadFileS3, deleteFileS3, deleteFilesS3 } = require('../../helpers/aws')
 
 exports.getPosts = async (req, res) => {
--- src/components/routes/posts.js
+++ src/components/routes/posts.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { getPosts, createPost, updatePost, deletePost, deletePosts } = require('../controllers/posts')
 const router = express.Router()
-const upload = require('../../helpers')
+const upload = require('../../helpers/storage')
 
 const auth = require('../../middleware/auth')
 
